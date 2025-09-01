c676b0ba6ff28b5e87581d54538f191d77cead82
sinadadsetan <dadsetan1382@gmail.com>
2025-09-01 13:10:24 +0330

    added parallax effect

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -14,4 +14,22 @@ export default function startGame(){
         k.add([k.sprite("platform"),k.pos(0,450),k.scale(4)]),
         k.add([k.sprite("platform"),k.pos(platfromWidth*4,450),k.scale(4)])
     ]
+
+    k.onUpdate(()=>{
+        if(bgPieces[1].pos.x<0){
+            bgPieces[0].moveTo(bgPieces[1].pos.x + bgwidth*2,0);
+            bgPieces.push(bgPieces.shift())
+        }
+
+        bgPieces[0].move(-100,0)
+        bgPieces[1].moveTo(bgPieces[0].pos.x+bgwidth*2,0)
+
+                if(platforms[1].pos.x<0){
+            platforms[0].moveTo(platforms[1].pos.x + platforms[1].width,450);
+            platforms.push(platforms.shift())
+        }
+
+        platforms[0].move(-2000,0)
+        platforms[1].moveTo(platforms[0].pos.x+ platforms[1].width*4,450)
+    })
 }

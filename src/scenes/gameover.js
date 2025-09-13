b3c7c5f12d32a71358a24847e4ba26f447c00b84
sinadadsetan <dadsetan1382@gmail.com>
2025-09-13 21:21:56 +0330

    improved the gameover scene

--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -1,11 +1,19 @@
 import k from "../ctx";
 export default function gameOver(){
     k.add([
-        k.pos(100,100),
-        k.text("game over")
+        k.anchor("center"),
+        k.pos(k.center().x,k.center().y),
+        k.text("Game Over",{font:"mania",size:100})
     ])
-
+    k.wait(2,()=>{
+        k.add([
+            k.anchor("center"),
+            k.pos(k.center().x,k.center().y+90),
+            k.text("press space or tap the screen to play again")
+        ])  
         k.onButtonDown("start",()=>{
             k.go("game")
         })
+    })
+
 }
